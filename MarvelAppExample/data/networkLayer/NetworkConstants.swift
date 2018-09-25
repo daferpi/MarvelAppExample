6a398a878d97705f6e7e13594a309b1e49ffd1b2
daferpi <daferpi@protonmail.ch>
2018-09-25 09:05:39 +0200

    add constants struct

---
+++ MarvelAppExample/data/networkLayer/NetworkConstants.swift
@@ -0,0 +1,13 @@
+//
+//  NetworkConstants.swift
+//  MarvelAppExample
+//
+//  Created by abelFernandez on 25/09/2018.
+//  Copyright © 2018 abelFernandez. All rights reserved.
+//
+
+import Foundation
+
+struct NetworkConstants {
+    static let API_KEY = "afed67d085831e707f8dc7ab639b641c"
+}
