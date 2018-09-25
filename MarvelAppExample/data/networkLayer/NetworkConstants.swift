c0cc870f17e4a7dd54ff8f8a6f97a7322ebc1d4e
daferpi <daferpi@protonmail.ch>
2018-09-25 09:21:32 +0200

    add api path for calls

--- MarvelAppExample/data/networkLayer/NetworkConstants.swift
+++ MarvelAppExample/data/networkLayer/NetworkConstants.swift
@@ -10,4 +10,12 @@ import Foundation
 
 struct NetworkConstants {
     static let API_KEY = "afed67d085831e707f8dc7ab639b641c"
+    static let BASE_URL = "https://gateway.marvel.com/"
+    
+    struct ApiPath {
+        static let CHARACTERS_LIST = "/v1/public/characters"
+        static let CHARACTERS_DETAIL = "/v1/public/characters/{characterId}"
+        static let COMICS_LIST = "/v1/public/comics"
+        static let COMICS_DETAIL = "/v1/public/comics/{comicId}"
+    }
 }
