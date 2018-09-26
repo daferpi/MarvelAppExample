99b676498996bf472c04b57047d7c53af36e95c0
daferpi <daferpi@protonmail.ch>
2018-09-26 09:30:15 +0200

    add api configuration

--- MarvelAppExample/data/networkLayer/ApiClient.swift
+++ MarvelAppExample/data/networkLayer/ApiClient.swift
@@ -13,6 +13,59 @@ enum ApiClient {
     case comicDetails(comicId:String)
 }
 
-//extension ApiClient:TargetType {
-//
-//}
+extension ApiClient:TargetType {
+
+    public var baseURL: URL {
+        return URL(string: NetworkConstants.BASE_URL)!
+    }
+
+    public var path: String {
+        switch self {
+        case .charactersList:
+            return NetworkConstants.ApiPath.CHARACTERS_LIST
+        case .characterDetail(let characterId):
+            return NetworkConstants.ApiPath.CHARACTERS_DETAIL + characterId
+        case .comicsList:
+            return NetworkConstants.ApiPath.COMICS_LIST
+        case .comicDetails(let comicId):
+            return NetworkConstants.ApiPath.COMICS_DETAIL + comicId
+        }
+    }
+
+    public var method: Moya.Method {
+        return .get
+    }
+
+    public var sampleData: Data {
+        switch self {
+        case .charactersList:
+            return "".utf8Encoded
+        case .characterDetail(let characterId):
+            return "".utf8Encoded
+        case .comicsList:
+            return "".utf8Encoded
+        case .comicDetails(let characterId):
+            return "".utf8Encoded
+        }
+    }
+
+    public var task: Task {
+        return .requestPlain
+    }
+
+    public var headers: [String: String]? {
+        return ["Content-type": "application/json"]
+    }
+
+}
+
+// MARK: - Helpers
+private extension String {
+    var urlEscaped: String {
+        return addingPercentEncoding(withAllowedCharacters: .urlHostAllowed)!
+    }
+
+    var utf8Encoded: Data {
+        return data(using: .utf8)!
+    }
+}
--- MarvelAppExample/data/networkLayer/NetworkConstants.swift
+++ MarvelAppExample/data/networkLayer/NetworkConstants.swift
@@ -14,8 +14,8 @@ struct NetworkConstants {
     
     struct ApiPath {
         static let CHARACTERS_LIST = "/v1/public/characters"
-        static let CHARACTERS_DETAIL = "/v1/public/characters/{characterId}"
+        static let CHARACTERS_DETAIL = "/v1/public/characters/"
         static let COMICS_LIST = "/v1/public/comics"
-        static let COMICS_DETAIL = "/v1/public/comics/{comicId}"
+        static let COMICS_DETAIL = "/v1/public/comics/"
     }
 }
